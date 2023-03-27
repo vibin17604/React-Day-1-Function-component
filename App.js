e09879f3e852f3a8c07ecdba399b3360ac421ea2
vibin17604 <108916451+vibin17604@users.noreply.github.com>
2023-03-27 10:25:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import World from './Day 1/Team';
+
+function App() {
+  return (
+    <div >
+      <World/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Team.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function World() {
+  return (
+    <div>
+        <center>
+        <p><b>This is Team Functional Component</b></p>
+        </center>
+    </div>
+  )
+}
+
+export default World
